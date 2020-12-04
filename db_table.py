00a6a1be8e782b5db20594168f55605743e3d459
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2020-12-04 10:29:28 +0100

    Update db_table.py

--- db_table.py
+++ db_table.py
@@ -26,3 +26,16 @@ RETURNING id;
 
 ALTER TABLE table_name 
 DROP COLUMN column_name;
+
+
+
+SELECT
+    a,
+    fruit_a,
+    b,
+    fruit_b
+FROM
+    basket_a
+LEFT JOIN basket_b 
+    ON fruit_a = fruit_b
+WHERE b IS NULL;
